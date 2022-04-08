05b7ef64ece0acc78924926b00a633dabbe5f0a8
Flow-matic <jonathonvinson22@gmail.com>
2022-04-08 02:19:22 +0000

    The reduce() method - part 2

--- reduce.js
+++ reduce.js
@@ -31,10 +31,26 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'Noah',
+    profession: 'Manager',
+    yrsExperience: 10
   }
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience)
 
 // Grouping by a property, and totaling it too
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+let key = curr.profession;
+if (!acc[key]) {
+  acc[key] = curr.yrsExperience;
+} else {
+  acc[key] += curr.yrsExperience;
+}
+return acc;
+}, {});
+console.log(experienceByProfession);
